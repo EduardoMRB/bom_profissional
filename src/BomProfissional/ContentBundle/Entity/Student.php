7fa7c722fb0ff5338ac61d339bcc4a6615895b1d
EduardoMRB <eduardomrb@gmail.com>
2012-12-29 00:58:06 -0300

    Adiciona entidades Student e repositorio StudentRepository

---
+++ src/BomProfissional/ContentBundle/Entity/Student.php
@@ -0,0 +1,463 @@
+<?php
+
+namespace BomProfissional\ContentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Student
+ *
+ * @ORM\Table("student")
+ * @ORM\Entity(repositoryClass="BomProfissional\ContentBundle\Entity\StudentRepository")
+ * @Assert\UniqueEntity("username")
+ * @Assert\UniqueEntity("email")
+ * @Assert\UniqueEntity("cpf")
+ */
+class Student
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\MaxLength(255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\MaxLength(255)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="passwd", type="string", length=255)
+     */
+    private $passwd;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="country", type="integer")
+     */
+    private $country;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cpf", type="string", length=255, unique=true)
+     */
+    private $cpf;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="telefone", type="string", length=50)
+     */
+    private $telefone;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="course", type="integer")
+     */
+    private $course;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="paymentDue", type="date")
+     */
+    private $paymentDue;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cep", type="string", length=50)
+     */
+    private $cep;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="district", type="string", length=100)
+     */
+    private $district;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Student
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return Student
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return Student
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set passwd
+     *
+     * @param string $passwd
+     * @return Student
+     */
+    public function setPasswd($passwd)
+    {
+        $this->passwd = $passwd;
+    
+        return $this;
+    }
+
+    /**
+     * Get passwd
+     *
+     * @return string 
+     */
+    public function getPasswd()
+    {
+        return $this->passwd;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Student
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return Student
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set country
+     *
+     * @param integer $country
+     * @return Student
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return integer 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set cpf
+     *
+     * @param string $cpf
+     * @return Student
+     */
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    
+        return $this;
+    }
+
+    /**
+     * Get cpf
+     *
+     * @return string 
+     */
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * Set telefone
+     *
+     * @param string $telefone
+     * @return Student
+     */
+    public function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+    
+        return $this;
+    }
+
+    /**
+     * Get telefone
+     *
+     * @return string 
+     */
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    /**
+     * Set course
+     *
+     * @param integer $course
+     * @return Student
+     */
+    public function setCourse($course)
+    {
+        $this->course = $course;
+    
+        return $this;
+    }
+
+    /**
+     * Get course
+     *
+     * @return integer 
+     */
+    public function getCourse()
+    {
+        return $this->course;
+    }
+
+    /**
+     * Set paymentDue
+     *
+     * @param \DateTime $paymentDue
+     * @return Student
+     */
+    public function setPaymentDue($paymentDue)
+    {
+        $this->paymentDue = $paymentDue;
+    
+        return $this;
+    }
+
+    /**
+     * Get paymentDue
+     *
+     * @return \DateTime 
+     */
+    public function getPaymentDue()
+    {
+        return $this->paymentDue;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     * @return Student
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+    
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string 
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set cep
+     *
+     * @param string $cep
+     * @return Student
+     */
+    public function setCep($cep)
+    {
+        $this->cep = $cep;
+    
+        return $this;
+    }
+
+    /**
+     * Get cep
+     *
+     * @return string 
+     */
+    public function getCep()
+    {
+        return $this->cep;
+    }
+
+    /**
+     * Set district
+     *
+     * @param string $district
+     * @return Student
+     */
+    public function setDistrict($district)
+    {
+        $this->district = $district;
+    
+        return $this;
+    }
+
+    /**
+     * Get district
+     *
+     * @return string 
+     */
+    public function getDistrict()
+    {
+        return $this->district;
+    }
+}
